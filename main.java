5fa9b5d756130ba7f8886c76ba7253d89b754618
EnriqueM11 <89398794+EnriqueM11@users.noreply.github.com>
2023-02-14 22:34:21 -0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,29 @@
+package ejercicio;
+
+import java.util.Stack;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 pop metodo = new pop(5);
+		 
+		 metodo.Push('8');
+		 metodo.Push('6');
+		 metodo.Push('4');
+		 metodo.Push('2');
+		 metodo.Push('1');
+		 
+		 for(int i=0; i<metodo.stack.length;i++) {
+			 System.out.println(metodo.stack[i]);
+		 }
+		 
+		 metodo.pop();
+		 
+		 
+		 
+
+	}
+
+}
